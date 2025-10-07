ee937da90d84c3b69454923bcdcb4026aff80060
Uriel Olayinka <umo204@nyu.edu>
2025-10-07 22:36:48 +0000

    Implemented filtering and sorting for RottenPotatoes with Cucumber step definitions

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,12 @@ Given(/the following movies exist/) do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(
+      title: movie['title'],
+      rating: movie['rating'],
+      release_date: Date.strptime(movie['release_date'], "%d-%b-%Y")
+    )
   end
-  pending "Fill in this step in movie_steps.rb"
 end
 
 Then(/(.*) seed movies should exist/) do |n_seeds|
@@ -15,32 +19,49 @@ end
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then(/I should see "(.*)" before "(.*)"/) do |_e1, _e2|
+Then(/I should see "(.*)" before "(.*)"/) do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  pending "Fill in this step in movie_steps.rb"
+  stuff = page.body
+  expect(stuff.index(e1)).to be < stuff.index(e2)
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When(/I (un)?check the following ratings: (.*)/) do |_uncheck, _rating_list|
+When(/I (un)?check the following ratings: (.*)/) do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+  ratings = rating_list.split(',').map(&:strip)
+  ratings.each do |rating|
+    if uncheck
+      uncheck("ratings_#{rating}")
+    else
+      check("ratings_#{rating}")
+    end
+  end
 end
 
 # Part 2, Step 3
-Then(/^I should (not )?see the following movies: (.*)$/) do |_no, _movie_list|
+Then(/^I should (not )?see the following movies: (.*)$/) do |no, movie_list|
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+  movies = movie_list.split(',').map(&:strip)
+  movies.each do |movie|
+    if no
+      expect(page).not_to have_content(movie)
+    else
+      expect(page).to have_content(movie)
+    end
+  end
 end
 
 Then(/I should see all the movies/) do
   # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
+  Movie.all.each do |movie|
+    expect(page).to have_content(movie.title)
+  end
 end
 
 ### Utility Steps Just for this assignment.
